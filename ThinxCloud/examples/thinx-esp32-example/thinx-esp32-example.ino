759abf7cbeb2f2c12e21967c717fe2a4075838f0
Martin <martinius96@gmail.com>
2019-01-06 00:01:37 +0100

    Update thinx-esp32-example.ino

--- ThinxCloud/examples/thinx-esp32-example/thinx-esp32-example.ino
+++ ThinxCloud/examples/thinx-esp32-example/thinx-esp32-example.ino
@@ -1,3 +1,9 @@
+/*|----------------------------------------------------------|*/
+/*|ThinX Cloud ENTERPRISE connection                         |*/
+/*|EMAIL: martinius96@gmail.com                              |*/
+/*|NOT TESTED OFFICIALY YET                                  |*/
+/*|CORE: June 2018                                           |*/
+/*|----------------------------------------------------------|*/
 #include <WiFi.h>
 #include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
 #include <Arduino.h>
