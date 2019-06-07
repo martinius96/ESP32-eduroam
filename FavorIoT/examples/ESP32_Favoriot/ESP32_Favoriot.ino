5ed7b124979ecc33c1f19366a447c78fda893742
Martin <martinius96@gmail.com>
2019-06-07 14:24:12 +0200

    Update ESP32_Favoriot.ino

--- FavorIoT/examples/ESP32_Favoriot/ESP32_Favoriot.ino
+++ FavorIoT/examples/ESP32_Favoriot/ESP32_Favoriot.ino
@@ -1,8 +1,12 @@
 #include <SPI.h>
 #include <WiFi.h>
+#include "esp_wpa2.h"
 
-char ssid[] = "your_network_SSID";      // change it!
-char pass[] = "your_network_password";  // change it!
+const char* ssid = "eduroam";
+#define EAP_ANONYMOUS_IDENTITY "anonymous@example.com"
+#define EAP_IDENTITY "nickname@example.com"
+#define EAP_PASSWORD "password"
+
 const String yourDevice = "deviceDefault@your_Username"; // change it!
 int status = WL_IDLE_STATUS;
 char server[] = "api.favoriot.com";
@@ -10,9 +14,14 @@ WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
-  WiFi.disconnect();
-  Serial.println("Connecting...");
-  WiFi.begin(ssid,pass);
+  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_ANONYMOUS_IDENTITY, strlen(EAP_ANONYMOUS_IDENTITY));
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+  esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config settings to default
+  esp_wifi_sta_wpa2_ent_enable(&config); //set config settings to enable function
+  WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
+  WiFi.mode(WIFI_STA); //init wifi mode
+  WiFi.begin(ssid); //connect to wifi
   while((!(WiFi.status() == WL_CONNECTED))){
     delay(300);
     Serial.print("...");
